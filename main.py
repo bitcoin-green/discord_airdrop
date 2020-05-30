49641799e2b9a1583160250adb41c5832044d29a
Nadro-J <n4dro@protonmail.com>
2020-05-30 01:55:49 +0000

    removing old code

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-import time
-import discord
-import asyncio
 import traceback
-import json
-import lib.utility_func as utility_func
-import lib.rpc_json as rpc_json
 from os import listdir
+import discord, asyncio
+import lib.rpc_json as rpc_json
 from discord.ext import commands
 from os.path import isfile, join
+import lib.utility_func as utility_func
 
-client = commands.Bot(command_prefix='$', description="Airdrop - Sending coins to all!")
-client.remove_command('help')
-thrdPartyAuthConfig = utility_func.load_json('./config/authenticate.json')
-messages = 0
-
-async def reset_message_activity(hrs):
-    while not client.is_closed():
-        try:
-            msg_reset_24hr = {'discord-user': []}
-            indent_data = json.dumps(msg_reset_24hr, indent=4)
-            utility_func.jsonfile('./data/message_activity_24hr.json', indent_data)
-            await asyncio.sleep(3600*hrs)
-        except Exception as error:
-            print (f"Exception raised: reset_message_activity()\nError:{error}")
+client = commands.Bot(command_prefix='$', description="Discord Airdrop bot")
+client.remove_command('help')  # replaced with $cmd
 
 async def currentBlockHeight():
     while not client.is_closed():
@@ -31,6 +16,7 @@ async def currentBlockHeight():
             await asyncio.sleep(60)
         except Exception as currBlockHeight_err:
             print (f"Exception raised: currentBlockHeight - {currBlockHeight_err}")
+            await asyncio.sleep(60)
 
 @client.event
 async def on_ready():
@@ -39,30 +25,6 @@ async def on_ready():
     print('[>>] %s' % (client.user.id))
     await client_extensions()
     client.loop.create_task(currentBlockHeight())
-
-@client.event
-async def on_message(message):
-    msgActivityLog = utility_func.load_json('./data/message_activity_24hr.json')
-    tmp_ids = []
-
-    for i in range(0, len(msgActivityLog['discord-user'])):
-        for id in msgActivityLog['discord-user'][i].keys():
-            tmp_ids.append(id)
-
-    if not utility_func.check_duplicate(str(message.author.id), tmp_ids):
-        msgActivityLog['discord-user'].append(({str(message.author.id):[{"messages": 1}]}))
-        update = json.dumps(msgActivityLog)
-        utility_func.jsonfile('./data/message_activity_24hr.json', update)
-    else:
-        if not '$join' in message.content:
-            #print (f"User: {message.author.id}\nMessage: {message.content}\n-------")
-            total_msg = msgActivityLog['discord-user'][tmp_ids.index(str(message.author.id))][str(message.author.id)][0]['messages']
-            total_msg += 1
-            msgActivityLog['discord-user'][tmp_ids.index(str(message.author.id))][str(message.author.id)][0]['messages'] = total_msg
-            update = json.dumps(msgActivityLog)
-            utility_func.jsonfile('./data/message_activity_24hr.json', update)
-
-    await client.process_commands(message)
 
 async def client_extensions():
     for extension in [f.replace(".py", "") for f in listdir("cogs") if isfile(join("cogs", f))]:
@@ -75,8 +37,4 @@ async def client_extensions():
             traceback.print_exc()
 
 if __name__ == '__main__':
-    try:
-        client.run(thrdPartyAuthConfig['discord-code'])
-    except BaseException:
-        time.sleep(10)
-        client.run(thrdPartyAuthConfig['discord-code'])
+    client.run(utility_func.load_json('./config/authenticate.json')['discord-code'])
